5f8972e6ef1e1b911f9834afa6625d1b27388f3f
mohitpachauri0095 <mohitpachauri095@gmail.com>
2025-10-31 13:56:32 +0530

    commit

---
+++ js/array/basic_array.js
@@ -0,0 +1,45 @@
+
+// // use this array
+
+// const a=[1,2,3,4,5,6,7, "baaf," , undefined, "c",1.2 ];
+
+
+// const b=new Array(8).fill(0);
+// console.log(b);
+
+
+// const c=new Array(8)
+// console.log(c);
+
+// const d=new Array(8).fill(0);
+// console.log(b);
+// a[9]=20;
+// console.log(a[8]);
+
+// const t= new Array("mohi0", "pachauri", "helly");
+// console.log(t) 
+
+
+// //method
+// //push pop
+
+// const m= [1,2,3,4,5,6,7];
+// // m[m.length]=22;
+// m[4]=20;
+
+// console.log(a);
+
+// const m= [1,2,3,4,5,6,7];
+// a.push(55)
+
+//pop
+
+const a= [1,2,3,4,5,6,7];
+const lastvalue =a.pop();
+console.log(lastvalue);
+console.log(a);
+
+
+// unsift value nikalana 
+// shift value dalna 
+
---
+++ js/array/shift & unshift.js
@@ -0,0 +1,5 @@
+const a=[1,2,3,4,5,6,7,8,];
+a.unshift (9);
+console.log(a)
+a.shift()
+console.log(a)
