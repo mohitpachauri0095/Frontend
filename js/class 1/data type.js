e23a1d9021a479ac4dfa75d328dad61c3c6df16e
mohitpachauri0095 <mohitpachauri095@gmail.com>
2025-10-31 12:17:09 +0530

    commit

---
+++ js/class 1/data type.js
@@ -0,0 +1,23 @@
+let num=50;
+console.log(num)
+
+// String data type
+let str="hello mohit ji"
+console.log(str)
+
+
+// boolean data type
+
+let bool= false;
+console.log(bool);
+
+//null
+let a= null;
+console.log(typeof a);
+
+// underfined
+
+let b;
+console.log(b)
+
+
