997e17a62eb43fb056ea21258dec44a5c211b65b
mohitpachauri0095 <mohitpachauri095@gmail.com>
2025-11-01 15:10:29 +0530

    commit

---
+++ js/type conversion/script.js
@@ -0,0 +1,23 @@
+// type of data type
+
+result='3';
+console.log(result);
+console.log(typeof result);
+
+resulto =9;
+console.log(resulto);
+console.log(typeof resulto);
+
+resulti ='7'-7;
+console.log(resulti);
+console.log(typeof resulti);
+
+
+// parseInt use for point value to zero convert
+
+
+
+resultp =678.54
+resultp=parseInt(resultp)
+console.log(resultp);
+console.log(typeof resultp)
