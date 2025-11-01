1d1068c08d3703eb7816d1bd4ab1d61fcd99b3a4
mohitpachauri0095 <mohitpachauri095@gmail.com>
2025-11-01 22:25:26 +0530

    commit

---
+++ js/operator/operator.js
@@ -0,0 +1,36 @@
+// arthmethic operator
+ 
+let a=9;
+let b=8;
+console.log(a+b);
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a**b)
+
+console.log(a%b)
+
+console.log (12+12)
+console.log (12-12)
+console.log (12*12)
+console.log (12/12)
+console.log (12**12)
+
+
+let m=5;
+console.log(m++);
+console.log(m);
+
+console.log(1>6);
+console.log(1>=6);
+console.log(1<6);
+console.log(1<=6);
+console.log(1<6);
+console.log(1==6);
+
+// logical operator
+let g=4;
+let h=5;
+console.log(g||h);
+console.log(g&&h);
+console.log(!g);
