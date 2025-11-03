357d1b9b1d15c0359d804c94aa459f56ce17d7cb
mohitpachauri0095 <mohitpachauri095@gmail.com>
2025-11-03 13:29:41 +0530

    commit

---
+++ js/work_shop_js/function_checknumber.js
@@ -0,0 +1,10 @@
+function chenumber(num){
+    if (num % 2 == 0){
+        return "true";
+
+    }else{
+        return"false";
+    }
+};
+   let res=chenumber(3);
+   console.log(res);
---
+++ js/work_shop_js/practice.js
@@ -0,0 +1,22 @@
+// var name ="Alice";
+// var name ="Bob";
+// console.log(name);
+
+// const sum = (Num, num2) => {
+//     return num + num2;
+// }
+
+
+// const double = num => num * 2;
+// if(num%2)
+
+function chenumber(num){
+    if (num % 2 == 0){
+        return "true";
+
+    }else{
+        return"false";
+    }
+};
+   let res=chenumber(3);
+   console.log(res);
