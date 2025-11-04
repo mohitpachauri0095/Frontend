440c7891d799f5c583a5d3a92ce7298f00c4d797
mohitpachauri0095 <mohitpachauri095@gmail.com>
2025-11-04 17:32:08 +0530

    commit

---
+++ js/array/Add and Remove Items.js
@@ -0,0 +1,10 @@
+let fruits =[" Apple", "banana", "mango"];
+fruits.push("Orange");
+fruits.unshift("Grapes");
+fruits.pop();
+fruits.shift();
+console.log(fruits);
+
+let fruit= [ "apple", "banana", "mango"];
+fruits[1] = "cherry";
+console.log(fruit);
