c2927469c973fc719277714151256f725b4ffcad
mohitpachauri0095 <mohitpachauri095@gmail.com>
2025-11-08 23:03:17 +0530

    commit

---
+++ js/condinal statement/switch.js
@@ -0,0 +1,9 @@
+let ch=1;
+switch (ch){
+    case 1:
+        console.log(" i m 1");
+        case 2:
+            console.log(" i m 2");
+            case 3:
+                console.log(" i m 3");
+}
