da260a8c8ca902c7b08b7269b72cfc58092b0754
mohitpachauri0095 <mohitpachauri095@gmail.com>
2025-11-13 23:10:07 +0530

    commit

---
+++ js/funshion/parameter.js
@@ -0,0 +1,9 @@
+function name(a){
+    console.log("hello mohi ji "+ a);
+}
+name ("you are great .");
+
+function name2(r){
+    console.log("No! no dude" +r);
+}
+name2(" i am not great brother. ")
