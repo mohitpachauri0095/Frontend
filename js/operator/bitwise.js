38e5e3044e83e19544441905f1f204cb839e3fd6
mohitpachauri0095 <mohitpachauri095@gmail.com>
2025-11-16 22:41:01 +0530

    commit

---
+++ js/operator/bitwise.js
@@ -0,0 +1,6 @@
+console.log(5 & 1);  // AND → 1
+console.log(5 | 1);  // OR  → 5
+console.log(5 ^ 1);  // XOR → 4
+console.log(~5);     // NOT → -6
+console.log(5 << 1); // Left shift → 10
+console.log(5 >> 1); // Right shift → 2
