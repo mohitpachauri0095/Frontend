83a42f616c9f8d38a21e674e553c9cb84d8f1ae8
mohitpachauri0095 <mohitpachauri095@gmail.com>
2025-11-20 19:52:46 +0530

    commit

---
+++ React Learning/vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return <div>
+    <p> Hello Mohit Ji </p>
+    <Button />
+    <Button />
+    <Button />
+    <Button />
+    <Button />
+    <Button />
+  </div>;
+}
+function Button(){
+  return <button> Click me</button>
+}
+
+
+export default App;
