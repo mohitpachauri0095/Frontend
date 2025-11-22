adbebb486ad5341764d3074c46fca66756a50305
mohitpachauri0095 <mohitpachauri095@gmail.com>
2025-11-22 21:11:17 +0530

    Add initial files for Task 3 and create a To-Do List HTML structure
    - Created sript.js and style.css for Task 3 with initial empty content.
    - Added Card component in React with a title and description.
    - Implemented a constructor function for creating person objects.
    - Established the basic structure for a To-Do List application in HTML.

--- React Learning/vite-project/src/App.jsx
+++ React Learning/vite-project/src/App.jsx
@@ -1,17 +1,12 @@
-function App() {
-  return <div>
-    <p> Hello Mohit Ji </p>
-    <Button />
-    <Button />
-    <Button />
-    <Button />
-    <Button />
-    <Button />
-  </div>;
+import Card from "./components/Card";
+import "./App.css";
+function App(){
+  return(
+  <div> 
+    <Card
+    title="Product 1"
+    discription ="abcdefghig"
+    </h1>App
+    </div>
 }
-function Button(){
-  return <button> Click me</button>
-}
-
-
 export default App;
---
+++ React Learning/vite-project/src/components/Card.jsx
@@ -0,0 +1,7 @@
+im
+function Card(){
+    return(
+        <h1> Card Title</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aperiam vitae debitis dicta sapiente enim fugit consequuntur fugiat sit nisi quas quia dolore quisquam ea, quos dolores, totam quam eius.</p>
+    )
+}
