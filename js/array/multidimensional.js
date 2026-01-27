b8502ee868792bc71aaa8396cf17720992c07a18
mohitpachauri0095 <mohitpachauri095@gmail.com>
2026-01-27 16:19:00 +0530

    commit

--- js/array/multidimensional.js
+++ js/array/multidimensional.js
@@ -3,9 +3,14 @@ let arr = [
     ["pachauri", 22],
     ["hello", 30]
 ]
-for ( let i=0; i<arr.length; i++){
-    for (let j=0; j<arr[i].length; j++){
-        console.log(arr[i][j]);
-    console.log(arr);
-    }
-}
+// for ( let i=0; i<arr.length; i++){
+//     for (let j=0; j<arr[i].length; j++){
+//         console.log(arr[i][j]);
+//     console.log(arr);
+//     }
+// }
+arr.forEach(data => {
+    data.forEach(val=> {
+        console.log(val);
+    })
+})
