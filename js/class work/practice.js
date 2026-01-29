35610816bcad2ef1da47dbcc4bd38a32abcc2d8c
mohitpachauri0095 <mohitpachauri095@gmail.com>
2026-01-29 22:26:10 +0530

    commit

--- js/class work/practice.js
+++ js/class work/practice.js
@@ -81,7 +81,7 @@
 // ----------------------------------------
 // EXPECTED USAGE
 // ----------------------------------------
-
+  
 // const result = analyzeScores(scores);
 // console.log(result);
  
