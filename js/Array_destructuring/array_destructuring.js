1b1e1b0adfbf1761451b0ab32677b86b03a9264f
mohitpachauri0095 <mohitpachauri095@gmail.com>
2026-02-07 00:08:37 +0530

    commit

---
+++ js/Array_destructuring/array_destructuring.js
@@ -0,0 +1,10 @@
+let array1 = ["Thomas", "chad", "mohit" ,"pachauri"]
+let item1=array1[0];
+let item2=array1[1];
+let item3=array1[2];
+let item4=array1[3];
+
+console.log(item1);
+console.log(item2);
+console.log(item3);
+console.log(item4);
