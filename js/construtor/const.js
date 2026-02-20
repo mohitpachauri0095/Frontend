939721668bb73c1debe735c3d1fc64c7c14c17c7
mohitpachauri0095 <mohitpachauri095@gmail.com>
2026-02-20 08:56:05 +0530

    commit

--- js/construtor/const.js
+++ js/construtor/const.js
@@ -2,7 +2,7 @@ const person = function (name, age) {
     this.firstName = name;
     this.age =age;
     this.birthyear =2025 - age;
-};       
+};        
 
 person.prototype.introduce = function(){
     console.log(`hi my name is &{this.firstName}, and i am ${this.age} year old, i was born in ${this.birthyear}`);
