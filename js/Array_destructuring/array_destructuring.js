836f01d9f3fe473719e1d6f083416dabd4d76390
mohitpachauri0095 <mohitpachauri095@gmail.com>
2026-03-06 17:20:54 +0530

    commit

--- js/Array_destructuring/array_destructuring.js
+++ js/Array_destructuring/array_destructuring.js
@@ -1,10 +1,17 @@
-let array1 = ["Thomas", "chad", "mohit" ,"pachauri"]
-let item1=array1[0];
-let item2=array1[1];
-let item3=array1[2];
-let item4=array1[3];
+// let array1 = ["Thomas", "chad", "mohit" ,"pachauri"]
+// let item1=array1[0];
+// let item2=array1[1];
+// let item3=array1[2];
+// let item4=array1[3];
 
+// console.log(item1);
+// console.log(item2);     
+// console.log(item3);   
+// console.log(item4);      
+
+
+let array2 = ["mohit", "pachauri", "hellow"]
+let item1=array2[0];
+let item2=array2[2];
 console.log(item1);
-console.log(item2);     
-console.log(item3);   
-console.log(item4);      
+console.log(item2);
