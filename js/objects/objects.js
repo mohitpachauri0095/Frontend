97eaeacfda92b21fe8f20ef64de7e2fdcdef2fa4
mohitpachauri0095 <mohitpachauri095@gmail.com>
2026-03-06 17:34:34 +0530

    commit

--- js/objects/objects.js
+++ js/objects/objects.js
@@ -1,7 +1,15 @@
-let obj = {
-    name : "mohit ji ",
-    age : 22,
-    location : "U.P",
-    contory : "INDIA"
+// let obj = {
+//     name : "mohit ji ",
+//     age : 22,
+//     location : "U.P",
+//     contory : "INDIA"
+// }
+// console.log(obj);
+
+let abc ={
+    name : "mohit pachauri ",
+    age :22,
+    location : "U.p",
+    contory : "InDIA"
 }
-console.log(obj);
+console.log(abc);
