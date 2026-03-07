e67b2e15fa86a583342037fe1a7a1b9645be7f70
mohitpachauri0095 <mohitpachauri095@gmail.com>
2026-03-07 17:33:41 +0530

    commit

--- js/function inside function/function.js
+++ js/function inside function/function.js
@@ -1,9 +1,46 @@
-function fun(){
+// function fun(){
+//     console.log("i am function 1");
+//     function fun2(){
+//         console.log(" i am fuction 2");
+//     }
+//     fun2();
+// }
+
+// fun();
+
+// function fun (){
+//     console.log("i am function");
+
+//     function fun2(){
+//         console.log("i am funtion2");
+//     }
+//     fun2()
+// }
+// fun();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function fun (){
     console.log("i am function 1");
+
     function fun2(){
-        console.log(" i am fuction 2");
+        console.log("i am function 2");
+
     }
     fun2();
 }
-
 fun();
