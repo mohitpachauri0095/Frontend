9e3597466c9288e61ace06c872f878238cf5dd28
mohitpachauri0095 <mohitpachauri095@gmail.com>
2026-03-07 13:24:50 +0530

    commit

---
+++ js/objects/obj_fun.js
@@ -0,0 +1,18 @@
+// let obj = {
+//     name : "Mohit pACHAURI",
+//     AGE : 33,
+//     fun: function () {
+//         console.log("I m Function")
+//     }, 
+//     arr:[20,30,40]
+// }
+// console.log(obj.fun);
+
+let obj = {
+    name : "MOhitJI",
+    age : 22,
+    location : " U.P. Macthura",
+    
+    }
+
+console.log(obj);
