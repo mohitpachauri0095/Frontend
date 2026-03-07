398c9e42c084fc2ce76fef5ccb9207b3197630a2
mohitpachauri0095 <mohitpachauri095@gmail.com>
2026-03-07 17:07:13 +0530

    commit

--- js/object inside array/obj_array.js
+++ js/object inside array/obj_array.js
@@ -1,7 +1,17 @@
+// let arr = [
+//     {user:1, name: "mohit"},
+//     {user:2, name: "rohit"},
+//     {user:3, name: "moni"},
+//     {user:4, name: "kamal"}
+// ]
+// console.log(arr);
+
+
 let arr = [
-    {user:1, name: "mohit"},
-    {user:2, name: "rohit"},
-    {user:3, name: "moni"},
-    {user:4, name: "kamal"}
+    {user:1, name: "mohit", age:22},
+    {user:2, name: "rajeshkhanna", age:22},
+    {user:3, name: "mohita", age:25},
+    {user:4, name: "rajeshkhannaa", age:252}
+
 ]
 console.log(arr);
