fe720fd12838cbfcaeff163469eb3ffe46105d6f
mohitpachauri0095 <mohitpachauri095@gmail.com>
2026-05-18 22:28:04 +0530

    commit

--- js/array/arr0.js
+++ js/array/arr0.js
@@ -1,9 +1,10 @@
 function sum(num1, num2, ...rest){
-let total =num1 +num2;   
+let total =num1 +num2;     
 for(let i=0; i< rest.length; i++){
     total +=rest[i]; 
 }
 return total;       
 }   
 
-console.log (sum (1,2,3,4,5,6,7,8,9,10));                  
+console.log (sum (1,2,3,4,5,6,7,8,9,10));  
+
