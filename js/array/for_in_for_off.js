8fdcbb8664cfc9659fbe239679c49fc31d2d7e3c
mohitpachauri0095 <mohitpachauri095@gmail.com>
2026-06-12 21:05:35 +0530

    commit

--- js/array/for_in_for_off.js
+++ js/array/for_in_for_off.js
@@ -3,14 +3,8 @@ let arr = [10, 20, 30, 40, 50];
 
 for (let ar in arr) {
     console.log(ar);      
-
-
-    
-
-
     for (let ar of arr) {
-        console.log(arr);      
-
+        console.log(arr);        
     }
 }                                     
 
