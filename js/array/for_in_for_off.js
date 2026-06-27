bba4cf77dd7797451004f4c0a5fcc2178de2255c
mohit <mohit@gmail.com>
2026-06-27 21:06:49 +0530

    commit

--- js/array/for_in_for_off.js
+++ js/array/for_in_for_off.js
@@ -1,7 +1,7 @@
 let arr = [10, 20, 30, 40, 50];  
 for (let ar in arr) {
     console.log(ar);      
-    for (let ar of arr) {   
+    for (let ar of arr) {       
         console.log(arr);        
     }
 }                                       
